bbc2f1dcd00498d3d592ba4d9e6f19c762641144
dir-FY24takahashierufu <erufu.takahashi.l431@dirbato.co.jp>
2024-04-05 14:40:51 +0900

    L2 chapter2終了

--- Sample/src/sample/Test.java
+++ Sample/src/sample/Test.java
@@ -2,10 +2,12 @@ package sample;
 
 public class Test { 
 
-    public static void main(String[] args) {
-        
-        System.out.println("kiramex");
+ // mainメソッドはプログラムの最初に動作します。
+    public static void main2(String[] args) {
+        /*
+       　System.out.println("kiramex");
         System.out.println("Java");
+        */
         System.out.println("学習を始めましょう!");
     }   
      
